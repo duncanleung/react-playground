ed5098add3ba2bb12f8543b0ee2b151f78cc7fcb
Duncan L <leungd@gmail.com>
2017-10-13 09:38:43 -0700

    Extracts Fade into HOC for toggle box example

--- react-animations/transition-group/toggle-box/src/App.js
+++ react-animations/transition-group/toggle-box/src/App.js
@@ -4,32 +4,56 @@ import { TweenMax } from "gsap";
 
 import "./App.css";
 
+const withFade = WrappedComponent => {
+  return class Fade extends Component {
+    componentWillEnter(callback) {
+      const ele = this.boxRef;
+
+      TweenMax.fromTo(
+        ele,
+        0.3,
+        { x: -100, opacity: 0 },
+        { x: 0, opacity: 1, onComplete: callback }
+      );
+    }
+
+    componentWillLeave(callback) {
+      const ele = this.boxRef;
+      TweenMax.fromTo(
+        ele,
+        0.3,
+        { x: 0, opacity: 1 },
+        { x: 100, opacity: 0, onComplete: callback }
+      );
+    }
+
+    render() {
+      return (
+        <WrappedComponent
+          {...this.props}
+          boxRef={boxRef => {
+            this.boxRef = boxRef;
+          }}
+        />
+      );
+    }
+  };
+};
+
 class Box extends Component {
-  componentWillEnter(callback) {
-    const ele = this.element;
+  constructor(props) {
+    super(props);
 
-    TweenMax.fromTo(
-      ele,
-      0.3,
-      { x: -100, opacity: 0 },
-      { x: 0, opacity: 1, onComplete: callback }
-    );
-  }
-
-  componentWillLeave(callback) {
-    const ele = this.element;
-    TweenMax.fromTo(
-      ele,
-      0.3,
-      { x: 0, opacity: 1 },
-      { x: 100, opacity: 0, onComplete: callback }
-    );
+    this.boxRef = this.props.boxRef;
+    this.props = this.props.rest;
   }
 
   render() {
-    return <div className="box" ref={node => (this.element = node)} />;
+    return <div className="box" ref={this.boxRef} {...this.rest} />;
   }
 }
+
+const FadeBox = withFade(Box);
 
 class App extends Component {
   state = {
@@ -46,7 +70,7 @@ class App extends Component {
     return (
       <div className="app">
         <ReactTransitionGroup>
-          {this.state.shouldShowBox && <Box />}
+          {this.state.shouldShowBox && <FadeBox />}
         </ReactTransitionGroup>
 
         <button className="toggle-btn" onClick={this.toggleBox}>
